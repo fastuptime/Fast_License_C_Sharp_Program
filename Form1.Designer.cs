06a97677a40d6af29cc0722e3cbdad737f6560e8
Can <63351166+fastuptime@users.noreply.github.com>
2024-06-18 15:30:12 +0300

    <3
    <3

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace softwareHome
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(228, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(592, 166);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check License";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(228, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(592, 98);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(221, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "License Key;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 636);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Last License C# - github.com/fastuptime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Management; // !!!! NUGET PAKET YÖNETİCİSİNDEN MANAGEMENT YÜKLEYİNİZ !!!!!!
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+
+namespace softwareHome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string GetLocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return localIP;
+        }
+
+        private string GetIpAddress()
+        {
+            string ipAddress = "?";
+            WebRequest request = WebRequest.Create("https://ipinfo.io/ip");
+            WebResponse response = request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                ipAddress = streamReader.ReadToEnd();
+            }
+            return ipAddress;
+        }
+
+        public string scriptID = "66716351446e28035842628f"; // Script ID
+        public string domain = "http://localhost:80/api/v1"; // Domain
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox1.Text == null || textBox1.Text == " ")
+            {
+                MessageBox.Show("Lütfen bir anahtar giriniz.");
+                return;
+            }
+
+            string json = JsonSerializer.Serialize(new
+            {
+                computerName = Environment.MachineName,
+                processorName = GetManagementObject("Win32_Processor", "Name"),
+                osName = GetManagementObject("Win32_OperatingSystem", "Caption"),
+                diskName = GetManagementObject("Win32_DiskDrive", "Model"),
+                localIP = GetLocalIPAddress(),
+                ip = GetIpAddress(),
+                action = "check",
+                key = textBox1.Text,
+                script = scriptID
+            });
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(domain);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = json.Length;
+
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                MessageBox.Show(result);
+            }
+
+        }
+
+        private void ListSystemInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Operating System Information:");
+            sb.AppendLine(GetManagementObject("Win32_OperatingSystem", "Caption"));
+            sb.AppendLine(GetManagementObject("Win32_OperatingSystem", "Version"));
+            sb.AppendLine(GetManagementObject("Win32_OperatingSystem", "Manufacturer"));
+            sb.AppendLine();
+
+            sb.AppendLine("Processor Information:");
+            sb.AppendLine(GetManagementObject("Win32_Processor", "Name"));
+            sb.AppendLine(GetManagementObject("Win32_Processor", "Manufacturer"));
+            sb.AppendLine(GetManagementObject("Win32_Processor", "Description"));
+            sb.AppendLine();
+
+            sb.AppendLine("Memory Information:");
+            sb.AppendLine(GetManagementObject("Win32_PhysicalMemory", "Capacity"));
+            sb.AppendLine(GetManagementObject("Win32_PhysicalMemory", "Speed"));
+            sb.AppendLine();
+
+            sb.AppendLine("Disk Drive Information:");
+            sb.AppendLine(GetManagementObject("Win32_DiskDrive", "Model"));
+            sb.AppendLine(GetManagementObject("Win32_DiskDrive", "InterfaceType"));
+            sb.AppendLine(GetManagementObject("Win32_DiskDrive", "MediaType"));
+            sb.AppendLine();
+
+            sb.AppendLine("Network Adapter Information:");
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                sb.AppendLine($"Name: {ni.Name}");
+                sb.AppendLine($"Description: {ni.Description}");
+                sb.AppendLine($"Status: {ni.OperationalStatus}");
+                sb.AppendLine($"Speed: {ni.Speed}");
+                sb.AppendLine();
+            }
+
+        }
+
+        private string GetManagementObject(string query, string property)
+        {
+            StringBuilder sb = new StringBuilder();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT {property} FROM {query}");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                sb.AppendLine($"{obj[property]}");
+            }
+            return sb.ToString();
+        }
+    }
+}
